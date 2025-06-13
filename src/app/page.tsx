e6696e62b8efb849e029b94b92ce3ecc0afd9ae7
sharad Gupta <shardgupta65@gmail.com>
2025-06-13 06:10:09 +0000

    image not show please check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,12 @@ export default function HomePage() {
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)] text-center p-4">
       <div className="mb-8">
         <Image
-          src="https://drive.google.com/file/d/18btWLzQJBaplrJUi6q2HFDAbQUREq6vf/view?usp=sharing"
+          src="https://placehold.co/600x400.png"
           alt="Person enjoying music with headphones and visual sound waves"
           width={600}
           height={400}
           className="rounded-lg shadow-xl"
+          data-ai-hint="headphones music"
           priority
         />
       </div>
